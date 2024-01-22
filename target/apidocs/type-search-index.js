337f9f3ced9b42fe39e8c1dbbb8f738c277ce77e
808issa808 <92200811+808issa808@users.noreply.github.com>
2024-01-22 11:46:04 +0600

    Add files via upload

---
+++ target/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.example","l":"Handler"},{"p":"com.example","l":"MyLogger"},{"p":"com.example","l":"SortingApp"}];updateSearchResults();
